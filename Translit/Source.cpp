805a52ce962456330b33ba7fc826b5b65bdc0924
SergeyStadnik <107566678+SergeyStadnik@users.noreply.github.com>
2022-06-20 11:44:21 +0300

    The end

--- Translit/Source.cpp
+++ Translit/Source.cpp
@@ -19,14 +19,64 @@ int main() {
 	int coll = 4;
 	int lenn = 33;
 	string text;
-	char Ru[] = { "a a a Жить хорошо, а хорошо жить еще лучше Privet kak dela chto delaesh' Ja Schuka" };
+	char Ru[200] = { " " };
 	char arr_Ru[4][33] = 
 	{ { 'А','Б','В','Г','Д','Е','Ё','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ъ','Ы','Ь','Э','Ю','Я'},
 	{ 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и','й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ','ы', 'ь', 'э', 'ю', 'я' },
-	{ 'A', 'B', 'V', 'G', 'D', 'E', 'Y', 'Zh', 'Z', 'I','Jj', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'H', 'C', '//', '// ', '//', '\"','Y', '//', ' //', '//', '//' },
-	{ 'a', 'b', 'v', 'g', 'd', 'e', 'y', 'zh', 'z', 'i','jj', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'c', '//', '// ', '//', '\"','y', '\'', '// ', '//', '//' } };
+	{ 'A', 'B', 'V', 'G', 'D', 'E', 'Y', '//', 'Z', 'I','//', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'H', 'C', '//', '// ', '//', '\"','Y', '//', ' //', '//', '//' },
+	{ 'a', 'b', 'v', 'g', 'd', 'e', 'y', '//', 'z', 'i','//', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'c', '//', '// ', '//', '\"','y', '\'', '// ', '//', '//' } };
+	string textRu = "Ru.txt";
+	string textEn = "En.txt";
+	string textNew = "New.txt";
+	int x = 0;
+	ifstream in_text_Ru(textRu);
+	ifstream in_text_En(textEn);
+	cout << "Перед вами программа транслитерации" << endl;
+	cout << "У вас есть два файла, один на русском, второй на английском" << endl;
+	cout << "Нажмите 1 чтобы перевести русский, и 2 чтобы перевести английский" << endl;
+	cin >> x;
+	if (x == 1)
+	{
+      if (in_text_Ru.fail())
+	{
+		cout << "error" << endl;
+		return 404;
+	}
+	while (getline(in_text_Ru, textRu))
+	{
+		for (size_t i = 0; i < textRu.length(); i++)
+		{
+			Ru[i] = textRu[i];
+		}
+	}
+	in_text_Ru.close();
+	}
+	else
+	{
+		if (in_text_En.fail())
+		{
+			cout << "error" << endl;
+			return 404;
+		}
+		while (getline(in_text_En, textEn))
+		{
+			for (size_t i = 0; i < textEn.length(); i++)
+			{
+				Ru[i] = textEn[i];
+			}
+		}
+		in_text_En.close();
+	}
+		for (size_t i = 0; i < size(Ru); i++)
+		{
+			if (Ru[i] == ' ')
+			{
+				text += Ru[i];
 
+			}
 
+	}
+	
 	for (size_t i = 0; i < size(Ru); i++)
 	{
 		if (Ru[i] == ' ')
@@ -165,6 +215,16 @@ int main() {
 				text += "Zh";
 				break;
 			}
+			if (Ru[i] == 'я')
+			{
+				text += "ya";
+				break;
+			}
+			if (Ru[i] == 'Я')
+			{
+				text += "Ya";
+				break;
+			}
 
 			for (size_t k = 0; k < lenn; k++)
 			{
@@ -196,7 +256,19 @@ int main() {
 		}
 
 	}
-cout <<text<< endl;
+
+     cout <<text<< endl;
+
+	 ofstream out_text_New;
+	 out_text_New.open(textNew);
+	 if (out_text_New.fail())
+	 {
+		 cout << "Error";
+		 return 404;
+	 }
+	 out_text_New << text;// записываю в текстовый документ
+	 out_text_New.close();
+
 
 
 	system("pause");
